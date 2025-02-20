831fa99e89f0c70641a6297ae7d55b799ab7491d
rupesh ranjeet borse <86816682+rupeshrb@users.noreply.github.com>
2025-02-20 19:08:07 +0530

    main

---
+++ replit.nix
@@ -0,0 +1,18 @@
+{pkgs}: {
+  deps = [
+    pkgs.which
+    pkgs.libpng
+    pkgs.libjpeg_turbo
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+  ];
+}
